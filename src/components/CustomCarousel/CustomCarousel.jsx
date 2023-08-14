768fee9cf4815cbead0b0da5eb7b8256d0ae19ef
TBSTheMountainsSay <blackswadow@yandex.ru>
2023-08-14 16:02:47 +0400

    Перед переносом слайдера в карусель

--- src/components/CustomCarousel/CustomCarousel.jsx
+++ src/components/CustomCarousel/CustomCarousel.jsx
@@ -1,36 +1,39 @@
 import React, { useState } from 'react';
 import styles from './CustomCarousel.module.scss';
 import src1 from 'src/images/Image6.png';
-import src2 from 'src/images/Image3.jpg';
-import src3 from 'src/images/Image4.jpg';
+import src2 from 'src/images/Image7.png';
+import src3 from 'src/images/Image9.png';
+import SvgSelector from '../SvgSelector/SvgSelector';
 
 const CustomCarousel = ({}) => {
-  const [images, setImages] = useState([src1, src1, src1]);
+  const [images, setImages] = useState([src2, src1, src3]);
 
   const handlePrev = () => {
+    const shiftedArr = images.slice(-1).concat(images.slice(0, -1));
+    setImages(shiftedArr);
+  };
+  const handleNext = () => {
     const shiftedArr = [...images];
     const firstItem = shiftedArr.shift();
     shiftedArr.push(firstItem);
     setImages(shiftedArr);
   };
-  const handleNext = () => {
-    const shiftedArr = images.slice(-1).concat(images.slice(0, -1));
-    setImages(shiftedArr);
-  };
 
   return (
     <div className={styles.customCarousel}>
-      <div className={styles.buttons}>
-        <button onClick={handlePrev} className={styles.button}>
-          Prev
-        </button>
-        <button onClick={handleNext} className={styles.button}>
-          Next
-        </button>
+      <div className={styles.wrapper}>
+        <div className={styles.buttons}>
+          <button onClick={handlePrev}>
+            <SvgSelector id={'arrow_left'} className={styles.arrow_left} />
+          </button>
+          <button onClick={handleNext}>
+            <SvgSelector id={'arrow_right'} className={styles.arrow_right} />
+          </button>
+        </div>
+        {images.map((image) => (
+          <img src={image} className={styles.image} />
+        ))}
       </div>
-      {images.map((image) => (
-        <img src={image} className={styles.image} />
-      ))}
     </div>
   );
 };
